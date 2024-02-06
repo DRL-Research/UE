1ba29a0b184f31a6c8cc85afce6b23eae8eca23d
ShaharBenIshay <Shahar.benshay@gmail.com>
2024-02-06 14:46:59 +0200

    just for test

--- cone_mapping.py
+++ cone_mapping.py
@@ -15,6 +15,8 @@ import struct
 
 decimation = 30e9  # Used to save an output image every X iterations.
 
+def say():
+    print('omri sleeps at DL')
 
 def aggregate_detections(airsim_client, iterations=1):
     pointcloud = np.array([])
