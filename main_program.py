391b23b95b4b22d377a3040d88674fa2d86c0f1b
shnoam <noamshamir23@gmail.com>
2024-06-17 16:02:18 +0300

    threading

--- main_program.py
+++ main_program.py
@@ -1,3 +1,5 @@
+import os
+import threading
 import cone_mapping
 import path_following
 import airsim
@@ -11,82 +13,6 @@ import time
 import turn_mapping
 import turn_helper
 
-
-# if __name__ == '__main__':
-#     # we should get the desired definitions of the simulator by the user: update setting.json & update airsim
-#     setup_manager = SetupManager()
-#     setup_manager.extract_cars()
-#     setup_manager.enableApiCarsControl()
-#     airsim_client = setup_manager.airsim_client
-#     time.sleep(1.0)
-#
-#     steering_procedure_manager = path_control.SteeringProcManager()
-#     moving_car_name = "Car2"
-#     airsim_client.confirmConnection()
-#
-#     airsim_manager = AirsimManager(airsim_client, setup_manager)
-#
-#     # Detect the cones and spline points, and return their location:
-#     print('Starting on-the-fly cone mapping with constant speed and steering procedure.')
-#     #mapping_data, pursuit_points = cone_mapping.mapping_loop(airsim_client)
-#     tracked_points, execution_time, curr_vel, transition_matrix = turn_mapping.mapping_loop(airsim_client,
-#                                                                                             moving_car_name,
-#                                                                                             setup_manager)
-#
-#     print('Mapping complete!')
-#
-#     # Stop until spline generation is complete:
-#     print('Stopping vehicle and generating a path to follow...')
-#     car_controls = airsim_client.getCarControls(vehicle_name=moving_car_name)
-#     car_controls.throttle = 0.0
-#     airsim_client.setCarControls(car_controls, vehicle_name=moving_car_name)
-#
-#     spline_obj = turn_helper.filter_tracked_points_and_generate_spline(tracked_points)
-#
-#     # Follow the spline using Stanley's method:
-#     print('Starting variable speed spline following procedure.')
-#     positions_lst = path_following.following_loop(airsim_client, spline_obj, execution_time, curr_vel,
-#                                                   transition_matrix, moving_car_name=moving_car_name)
-#     # plots_utils.combine_plot(spline_obj.xi,spline_obj.yi,positions_lst)
-#
-#     print('Full process complete! stopping vehicle.')
-#     car_controls.throttle = 0.0
-#     car_controls.brake = 1.0
-#     airsim_client.setCarControls(car_controls, vehicle_name=moving_car_name)
-#     steering_procedure_manager.terminate_steering_procedure()
-
-
-    # # Record the start time
-    # start_time = time.time()
-    #
-    # # Print the current time
-    # current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    # print(f'Current time: {current_time}')
-    #
-    # car_state = airsim_client.getCarState()
-    # position = car_state.kinematics_estimated.position
-    # print(f'start vehicle position: ({position.x_val}, {position.y_val}, {position.z_val})')
-    #
-    # target_elapsed_time = 20
-    # while True:
-    #     elapsed_time = time.time() - start_time
-    #     if elapsed_time >= target_elapsed_time:
-    #         break
-    #     # time.sleep(1)  # Sleep for 1 second before checking again
-    #
-    # # Print the time after the elapsed time
-    # current_time_after_elapsed = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    # print(f'Time after {target_elapsed_time} seconds: {current_time_after_elapsed}')
-    # car_state = airsim_client.getCarState()
-    # position = car_state.kinematics_estimated.position
-    # print(f'after 20 seconds vehicle position: ({position.x_val}, {position.y_val}, {position.z_val})')
-    #
-    # # Stop the vehicle:
-    # car_controls = airsim_client.getCarControls()
-    # car_controls.throttle = 0.0
-    # car_controls.brake = 1.0
-    # airsim_client.setCarControls(car_controls)
-    # steering_procedure_manager.terminate_steering_procedure()
 
 def run_for_car(moving_car_name, airsim_client, setup_manager):
     steering_procedure_manager = path_control.SteeringProcManager()
@@ -117,6 +43,7 @@ def run_for_car(moving_car_name, airsim_client, setup_manager):
     car_controls.brake = 1.0
     airsim_client.setCarControls(car_controls, vehicle_name=moving_car_name)
     steering_procedure_manager.terminate_steering_procedure()
+
 if __name__ == '__main__':
     # Perform the setup once
     setup_manager = SetupManager()
@@ -129,22 +56,39 @@ if __name__ == '__main__':
     airsim_client.confirmConnection()
 
     airsim_manager = AirsimManager(airsim_client, setup_manager)
+    print("ID of process running main program: {}".format(os.getpid()))
 
+    print("Main thread name: {}".format(threading.current_thread().name))
 
+    """
+    option 1 - works
+    """
 
-    # List of car names to run simultaneously
-    moving_car_names = ["Car1"]
-    threads = []
 
     # Create and start a thread for each car
-    for car_name in moving_car_names:
-        run_for_car(car_name, airsim_client, setup_manager)
-    #     thread = threading.Thread(target=run_for_car, args=(car_name, airsim_client, setup_manager))
-    #     threads.append(thread)
-    #     thread.start()
-    #
-    # # Wait for all threads to finish
-    # for thread in threads:
-    #     thread.join()
 
-    #print('All cars have completed their tasks.')
+    thread1 = threading.Thread(target=run_for_car, args=("Car2", airsim_client, setup_manager))
+    thread2 = threading.Thread(target=run_for_car, args=("Car4", airsim_client, setup_manager))
+
+    thread1.start()
+    time.sleep(0.1)    ## needed so the threads will not access the buffer simultaneously - something with tornado package
+    thread2.start()
+
+    # Wait for all threads to finish
+    thread1.join()
+    thread2.join()
+    """
+      option 2
+    """
+    # # Create a thread pool executor
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    #     # Submit tasks to the thread pool
+    #     futures = [executor.submit(run_for_car, car_name, airsim_client, setup_manager) for car_name in
+    #                moving_car_names]
+    #
+    #     # Wait for all tasks to complete
+    #     concurrent.futures.wait(futures)
+
+    print('All cars have completed their tasks.')
+
+
